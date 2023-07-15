5876e2b8da9f79ddcd34eee2cef5bb7a9a44cd15
Hanseway001 <wayne.hansen9276@stu.mtec.edu>
2023-07-14 20:44:25 -0600

    db connection working still cant get info from db

--- server/config/database.js
+++ server/config/database.js
@@ -1,11 +1,11 @@
 const { Pool } = require('pg');
-// const pool = new Pool({
-//     connectionString: process.env.DATABASE_URL
-// });
-
-// pool.connect();
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL
+});
+// console.log("hello" + process.env.DATABASE_URL)
+pool.connect();
 const getUsers = (req, res) => {
-    pool.query('SELECT * from user limit 5', (err, results) => {
+    pool.query('SELECT * from users', (err, results) => {
         if (err) throw err;
         for (let row of results.rows) {
             console.log(JSON.stringify(row))
