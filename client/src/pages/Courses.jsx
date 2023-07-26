cbe037cf727538530f2ab33355307594aa538b0b
Tegan <teganator275@gmail.com>
2023-07-26 09:47:04 -0600

    login css

--- client/src/pages/Courses.jsx
+++ client/src/pages/Courses.jsx
@@ -1,4 +1,6 @@
 import { Link } from "react-router-dom";
+import HomePageNavbar from "../components/HomePageNavBar";
+
 
 export const styles = {
   link: {
@@ -10,36 +12,7 @@ export const styles = {
 export function Courses() {
   return (
     <div className="App">
-      <header>
-
-        <div className="regHeader row, center, spacing">
-          <div className="row, center">
-            <img class="regIcon" src="src/images/iconL.png" alt="" />
-            <p class="titleSize white">Capstone</p>
-            <div className="regSearchbar center">
-              <img className="searchIcon" src="src/images/searchIcon.png" alt="search icon" />
-              <input className="navInput" type="text" placeholder="Search" />
-            </div>
-          </div>
-
-          <div className="row, center">
-            <img class="userImg" src="src/images/user.png" alt="user" />
-            <h5 className="white">Nicolas M.</h5>
-          </div>
-        </div>
-
-        <div className="regLinkContainer">
-          <Link style={styles.link} to={'/Courses'}>
-            Courses
-          </Link>
-          <Link className="white" style={styles.link} to={'/Registration'}>
-            Registration
-          </Link>
-          <Link style={styles.link} to={'/About'}>
-            About
-          </Link>
-        </div>
-      </header>
+      <HomePageNavbar />
 
       <img src="images/logo.png"  alt="" />
       <h1>LOGIN</h1>
